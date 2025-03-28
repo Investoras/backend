8cf260627ceb34c277ec9fe56d6edb0ae486085c
kormakz <kms.kornilov@gmail.com>
2025-03-28 22:52:13 +0300

    Модели пользователя и аккаунта

---
+++ Investoras_Backend/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System.Transactions;
+
+namespace Investoras_Backend.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
