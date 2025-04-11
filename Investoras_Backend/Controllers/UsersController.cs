276b2b8abee1dc3721eb0ab66ef4e7a1f13b56ad
kormakz <kms.kornilov@gmail.com>
2025-04-11 23:08:16 +0300

    Создал основные контроллеры для User

---
+++ Investoras_Backend/Controllers/UsersController.cs
@@ -0,0 +1,72 @@
+﻿using Investoras_Backend.Data;
+using Investoras_Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Investoras_Backend.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly ApplicationDbContext dbContext;
+
+    public UsersController(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    [HttpGet]
+    public IActionResult GetAllUsers()
+    {
+        var allImages = dbContext.Users.ToList();
+        return Ok(allImages);
+    }
+    [HttpPost]
+    public IActionResult AddUser(UpdateUser AddUser)
+    {
+        var user = new User()
+        {
+            Username = AddUser.Username,
+            Email = AddUser.Email,
+            Password = AddUser.Password
+        };
+
+
+        dbContext.Users.Add(user);
+        dbContext.SaveChanges();
+        return Ok(user);
+    }
+    [HttpPut]
+    [Route("{id:int}")]
+    public IActionResult UpdateUser(int id, UpdateUser UpdateUser)
+    {
+        var user = dbContext.Users.Find(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.Username = UpdateUser.Username;
+        user.Password = UpdateUser.Password;
+        user.Email = UpdateUser.Email;
+        dbContext.SaveChanges();
+        return Ok(user);
+    }
+    [HttpDelete]
+    [Route("{id:int}")]
+    public IActionResult DeleteUser(int id)
+    {
+        var user = dbContext.Users.Find(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        dbContext.Users.Remove(user);
+        dbContext.SaveChanges();
+        return Ok();
+    }
+}
+
+
---
+++ Investoras_Backend/Models/UpdateUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Investoras_Backend.Models
+{
+    public class UpdateUser
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
